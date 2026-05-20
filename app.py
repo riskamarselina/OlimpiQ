a81ef35abe6484a89d01d69e681eb906f55095c1
riskamarselina <riskamarselina19@students.unnes.ac.id>
2026-05-20 17:43:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
 =============================================================
   SISTEM SELEKSI OLIMPIADE MATEMATIKA
-  File: app/app.py
+  File: app.py
   Deskripsi: Aplikasi Streamlit untuk prediksi kesiapan
              olimpiade matematika siswa berbasis ML
 =============================================================
